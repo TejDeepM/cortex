37dafba2748b7e9d25bcdb1814f4e015100abf15
TejDeepM <tejdeepm30@gmail.com>
2024-02-06 08:24:00 +0400

    Model now remains in the context
    Press "RESET" button to refresh the bot

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 
 import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
 
-var input = document.getElementById("mainTextInput")
-input.addEventListener("keydown", runScript)
-input.class = ""
+const MODEL_NAME = "gemini-pro";
+const API_KEY = "AIzaSyDVigTO9l-21jvtAidvPC29SV2aYcxVlqY";
 
-document.getElementById("mainButton").addEventListener("click", check)
+var input = document.getElementById("mainTextInput")
+var sendButton = document.getElementById("mainButton")
+
+input.addEventListener("keydown", runScript)
+document.getElementById("resetButton").addEventListener("click", reset)
+sendButton.addEventListener("click", check)
+input.class = ""
 
 let Query = window.location["href"].split("?q=")[1]
 if (Query != undefined) {
@@ -24,6 +29,41 @@ function check() {
     }
 }
 
+var History = []
+function reset() {
+    History = []
+    document.getElementById("responseText").textContent = "Reset Succesfully"
+    document.getElementById("responseText").style.color = "rgb(0, 255, 0)"
+}
+
+reset()
+
+const generationConfig = {
+    temperature: 0.9,
+    topK: 1,
+    topP: 1,
+    maxOutputTokens: 2048,
+};
+
+const safetySettings = [
+    {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    },
+    {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    },
+    {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    },
+    {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    },
+    ];
+
 function mainFunc(event, query = "") {
 
     document.body.style.backgroundColor = "rgb(30, 30, 30)";
@@ -34,45 +74,17 @@ function mainFunc(event, query = "") {
     document.getElementById("loadingIcon").className = "animate__animated animate__zoomIn"
     document.getElementById("mainButton").className = "animate__animated animate__jello"
     
-    const MODEL_NAME = "gemini-pro";
-    const API_KEY = "AIzaSyDVigTO9l-21jvtAidvPC29SV2aYcxVlqY";
-    
     async function runChat() {
-        const genAI = new GoogleGenerativeAI(API_KEY);
-        const model = genAI.getGenerativeModel({ model: MODEL_NAME });
-    
-        const generationConfig = {
-        temperature: 0.9,
-        topK: 1,
-        topP: 1,
-        maxOutputTokens: 2048,
-        };
-    
-        const safetySettings = [
-        {
-            category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-            threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-        },
-        {
-            category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-            threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-        },
-        {
-            category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-            threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-        },
-        {
-            category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-            threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-        },
-        ];
-    
-        const chat = model.startChat({
-        generationConfig,
-        safetySettings,
-        history: [
-        ],
+
+        var genAI = new GoogleGenerativeAI(API_KEY);
+        var model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+        var chat = model.startChat({
+            history: History,
+            generationConfig: generationConfig,
+            safetySettings: safetySettings,
         });
+
 
         function removeDoubleAsterisks(text) {
             return text.replace(/\*\*/g, '');
@@ -91,8 +103,8 @@ function mainFunc(event, query = "") {
             toAdd = query
         }
 
-        const msg = toAdd + document.getElementById("mainTextInput").value;
-        const result = await model.generateContentStream(msg);
+        var msg = document.getElementById("mainTextInput").value;
+        var result = await chat.sendMessageStream(toAdd + msg);
         
         document.getElementById("responseText").className = "animate__animated animate__zoomOut";
         document.getElementById("responseText").className = "animate__animated animate__zoomIn";
@@ -100,12 +112,16 @@ function mainFunc(event, query = "") {
         document.getElementById("loadingIcon").hidden = true;
         document.getElementById("mainButton").className = ""
 
-        let text = '';
+        var text = ""
+
         for await (const chunk of result.stream) {
             const chunkText = chunk.text();
             text += removeDoubleAsterisks(chunkText);
-            document.getElementById("responseText").textContent = text
+            document.getElementById("responseText").textContent = text;
         }
+
+        History.push({role: "user", parts: msg})
+        History.push({role: "model", parts: text})
 
     }
 
@@ -113,6 +129,7 @@ function mainFunc(event, query = "") {
         document.getElementById("responseText").textContent = error.message
         document.body.style.backgroundColor = "rgb(40, 0, 0)";
         document.getElementById("responseText").style.color = "rgb(255, 150, 150)";
+        console.log(error)
     }
 
     runChat().catch(handleError);
